cb68bd97020e08f319f6f9c027af7647af34747c
John Okely <john@moodle.com>
2016-02-16 13:23:05 +0800

    Move breadcrumbs back, remove context header

--- renderers/core_renderer.php
+++ renderers/core_renderer.php
@@ -247,6 +247,16 @@ class theme_shiny_core_renderer extends core_renderer {
             return html_writer::tag('li', $link, $params);
         }
     }
+    public function full_header() {
+        $html = html_writer::start_tag('header', array('id' => 'page-header', 'class' => 'clearfix'));
+        $html .= html_writer::start_div('clearfix', array('id' => 'page-navbar'));
+        $html .= html_writer::tag('nav', $this->navbar(), array('class' => 'breadcrumb-nav'));
+        $html .= html_writer::div($this->page_heading_button(), 'breadcrumb-button');
+        $html .= html_writer::end_div();
+        $html .= html_writer::tag('div', $this->course_header(), array('id' => 'course-header'));
+        $html .= html_writer::end_tag('header');
+        return $html;
+    }
 }
 
 /**

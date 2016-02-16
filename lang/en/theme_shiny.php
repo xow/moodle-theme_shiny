e1e08223e739500c4961dd081ee1373f91d9128c
John Okely <john@moodle.com>
2016-02-16 15:12:11 +0800

    Fix styling, blocks only on left

--- lang/en/theme_shiny.php
+++ lang/en/theme_shiny.php
@@ -25,12 +25,12 @@
 $string['choosereadme'] = '
 <div class="clearfix">
 <div class="well">
-<h2>Clean</h2>
+<h2>Shiny</h2>
 <p><img class=img-polaroid src="shiny/pix/screenshot.jpg" /></p>
 </div>
 <div class="well">
 <h3>About</h3>
-<p>Clean is a modified Moodle bootstrap theme which inherits styles and renderers from its parent theme.</p>
+<p>Shiny is a modified Moodle bootstrap theme which inherits styles and renderers from its parent theme.</p>
 <h3>Parents</h3>
 <p>This theme is based upon the Bootstrap theme, which was created for Moodle 2.5, with the help of:<br>
 Stuart Lamour, Mark Aberdour, Paul Hibbitts, Mary Evans.</p>
@@ -45,7 +45,7 @@ Website: <a href="http://www.basbrands.nl">www.basbrands.nl</a>
 <p><a href="shiny/README.txt">How to copy and customise this theme.</a></p>
 </div></div>';
 
-$string['configtitle'] = 'Clean';
+$string['configtitle'] = 'Shiny';
 
 $string['customcss'] = 'Custom CSS';
 $string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
@@ -59,7 +59,7 @@ $string['invertdesc'] = 'Swaps text and background for the navbar at the top of 
 $string['logo'] = 'Logo';
 $string['logodesc'] = 'The logo is only displayed in the header of the front page and login page.<br /> If the height of your logo is more than 75px add div.logo {height: 100px;} to the Custom CSS box below, amending accordingly if the height is other than 100px.';
 
-$string['pluginname'] = 'Clean';
+$string['pluginname'] = 'Shiny';
 
 $string['region-side-post'] = 'Right';
 $string['region-side-pre'] = 'Left';
